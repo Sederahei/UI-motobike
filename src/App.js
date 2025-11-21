61cfcc3a05ab45927878f89b0861fad4a283a514
sederahei <hei.sedera.167@gmail.com>
2025-11-21 17:04:09 +0300

    refact:create the page navigate bare modern

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+
+import Home from "./pages/Home";
+import CommandesPage from "./pages/CommandesPage";
+import ProduitsPage from "./pages/ProduitsPage";
+import PanierPage from "./pages/PanierPage";
+import ClientsPage from "./pages/ClientsPage";
+import HistoriquePage from "./pages/HistoriquePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* ✅ Barre de navigation moderne */}
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            🚀 MotoBike App
+          </Typography>
+          <Button color="inherit" component={Link} to="/">Accueil</Button>
+          <Button color="inherit" component={Link} to="/produits">Produits</Button>
+          <Button color="inherit" component={Link} to="/panier">Panier</Button>
+          <Button color="inherit" component={Link} to="/commandes">Commandes</Button>
+          <Button color="inherit" component={Link} to="/clients">Clients</Button>
+          <Button color="inherit" component={Link} to="/historique">Historique</Button>
+        </Toolbar>
+      </AppBar>
+
+      {/* ✅ Routing vers les pages */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/produits" element={<ProduitsPage />} />
+        <Route path="/panier" element={<PanierPage />} />
+        <Route path="/commandes" element={<CommandesPage />} />
+        <Route path="/clients" element={<ClientsPage />} />
+        <Route path="/historique" element={<HistoriquePage />} />
+      </Routes>
+    </Router>
   );
 }
 
